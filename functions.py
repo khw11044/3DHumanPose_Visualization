96e097a17d3bb2d6310e895c961e41f8c4104956
khw11044 <khw11044@naver.com>
2023-01-11 14:44:31 +0900

    first commit

---
+++ functions.py
@@ -0,0 +1,95 @@
+import cv2
+import numpy as np
+H36M_JOINTMAP = [
+    [0, 1], [1, 2], [2, 3],       # 오른쪽 하체
+    [0, 4], [4, 5], [5, 6],        # 왼쪽 하체
+    [7, 13], [13, 14], [14, 15],     # 오른쪽 상체
+    [7, 10], [10, 11], [11, 12],     # 왼쪽 하체
+    [0, 7], [7, 8], [8, 9],          # 중앙 상체
+]
+
+MPII_JOINTMAP = [
+    [0, 1], [1, 2], [2, 6],     # 오른쪽 하체
+    [6, 3], [3, 4], [4, 5],      # 왼쪽 하체
+    [10, 11], [11, 12], [12, 7],
+    [7, 13], [13, 14], [14, 15],
+    [6, 7], [7, 8], [8, 9],      # 중앙 상체
+]
+
+color_box = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+
+
+def vis_2D_pose(frame, denorm_2d_pose, data_type):
+    x_set, y_set = denorm_2d_pose.T
+
+    if data_type == 'h36m':
+        JOINTMAP = H36M_JOINTMAP
+        root_joint_numer = 0
+    else:
+        JOINTMAP = MPII_JOINTMAP
+        root_joint_numer = 6
+
+    for j in range(len(JOINTMAP)):
+
+        child = tuple([int(x_set[JOINTMAP[j][0]]), int(y_set[JOINTMAP[j][0]])])
+        parent = tuple([int(x_set[JOINTMAP[j][1]]),
+                       int(y_set[JOINTMAP[j][1]])])
+        if j in [0, 1, 2, 6, 7, 8]:          # 오른쪽
+            color = color_box[0]
+        elif j in [3, 4, 5, 9, 10, 11]:       # 왼쪽
+            color = color_box[2]
+        else:
+            color = color_box[1]        # 중앙
+        # color = (0, 64 + 192 * (len(JOINTMAP) - j) / (len(JOINTMAP) - 1), 64 + 192 * j / (len(JOINTMAP) - 1))
+
+        size = 20
+        lsize = 20
+        cv2.line(frame, child, parent, color, lsize)
+        # cv2.circle(frame, parent, size, (0, 0, 0), -1)
+    # for jnum in range(denorm_2d_pose.shape[-1]):
+    #     #jnum = 10
+    #     cv2.circle(frame, (int(x_set[jnum]), int(
+    #         y_set[jnum])), size, (0, 255, 255), -1)
+    return frame
+
+
+def show3Dpose(annot, ax, radius=40, data_type='h36m', lcolor='red', rcolor='#0000ff', angles=(10, -60)):
+    l_size = 6
+    if data_type == 'h36m':
+        JOINTMAP = H36M_JOINTMAP
+        root_joint_numer = 0
+    else:
+        JOINTMAP = MPII_JOINTMAP
+        root_joint_numer = 6
+
+    for ind, (i, j) in enumerate(JOINTMAP):
+        if ind in [0, 1, 2, 6, 7, 8]:          # 오른쪽
+            color = 'b'
+        elif ind in [3, 4, 5, 9, 10, 11]:       # 왼쪽
+            color = 'r'
+        else:
+            color = 'g'        # 중앙
+
+        x, z, y = [np.array([annot[i, c], annot[j, c]]) for c in range(3)]
+        ax.plot(x, y, -z, lw=l_size, c=color)
+
+    RADIUS = radius  # space around the subject
+
+    xroot, yroot, zroot = annot[root_joint_numer,
+                                0], annot[root_joint_numer, 1], annot[root_joint_numer, 2]
+
+    ax.set_xlim3d([-RADIUS, RADIUS])
+    ax.set_ylim3d([-RADIUS, RADIUS])
+    ax.set_zlim3d([-RADIUS, RADIUS])
+
+    # ax.set_xlim3d([-RADIUS + xroot, RADIUS + xroot])
+    # ax.set_ylim3d([-RADIUS + yroot, RADIUS + yroot])
+    # ax.set_zlim3d([-RADIUS + zroot, RADIUS + zroot])
+    ax.view_init(angles[0], angles[-1])
+    # ax.set_xlabel("x")
+    # ax.set_ylabel("y")
+    # ax.set_zlabel("z")
+
+    # ax.set_xticklabels([])
+    # ax.set_yticklabels([])
+    # ax.set_zticklabels([])
---
+++ main.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+from pose_data import pose2D_1, pose3D_1, pose3Dgt_1
+from functions import *
+import matplotlib.pyplot as plt
+
+
+def final_visualization(pose_3D_pred, pose_3D_gt, arr, Radius, angles, save_img_folder):
+
+    stepX, stepZ = Radius*2 / 256, Radius*2 / 256
+    X1 = np.arange(-Radius, Radius, stepX)
+    Z1 = np.arange(-Radius, Radius, stepZ)
+    X1, Z1 = np.meshgrid(X1, Z1)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax_3D_pred = fig.add_subplot(1, 1, 1, projection='3d')
+
+    ax_3D_pred.plot_surface(X1, np.atleast_2d(
+        Radius), Z1, rstride=1, cstride=1, facecolors=arr)
+
+    ax_3D_pred.set_xlim3d([-Radius, Radius])
+    ax_3D_pred.set_ylim3d([-Radius, Radius])
+    ax_3D_pred.set_zlim3d([-Radius, Radius])
+    ax_3D_pred.set_xticklabels([])
+    ax_3D_pred.set_yticklabels([])
+    ax_3D_pred.set_zticklabels([])
+    ax_3D_pred.view_init(angles[0], angles[1])
+    # ax_3D_pred.grid(False)
+
+    pred3D = pose_3D_pred - np.array([0, 0, 530])
+    gt3D = pose_3D_gt - np.array([0, 0, 530])
+    show3Dpose(pred3D, ax_3D_pred, data_type='mpii',
+               radius=Radius, lcolor='black', angles=angles)      # 20,-70
+    # show3Dpose_with_annot2(gt3D, pred3D, ax_3D_pred, data_type='mpii', radius=Radius, lcolor='blac)
+
+    print('drawing....')
+    plt.draw()
+    i = 0
+    plt.savefig(save_img_folder + '/%05d.png' % (i), transparent=True)
+    #plt.savefig(config.save_img_folder + '/%05d.png'% (i))
+    plt.pause(0.01)
+    plt.show()
+    fig.clear()
+
+
+def main(img_path, pose2D, pose3D, pose3D_gt, save_img_folder):
+
+    img = cv2.imread(img_path)
+    img = vis_2D_pose(img, pose2D, data_type='mpii')
+    img = cv2.resize(img, (256, 256))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    arr = img / 255
+    arr = cv2.flip(arr, 0)
+
+    Radius = 200
+    angles = (10, -70)
+
+    final_visualization(pose3D, pose3D_gt, arr,
+                        Radius, angles, save_img_folder)
+
+
+if __name__ == '__main__':
+    img_path = './mpi_inf_3dhp_test_set/TS2/imageSequence/000001.jpg'
+    pose2D = pose2D_1
+    pose3D = pose3D_1
+    pose3D_gt = pose3Dgt_1
+    save_img_folder = './output'
+    main(img_path, pose2D, pose3D, pose3D_gt, save_img_folder)
